0b6dcd31be1fa7d96861d6339e9c4de0d75190e6
Naza Gomez <nazasanchez12.26@gmail.com>
2023-11-16 21:02:20 -0600

    Commit-Naza

--- admin/main.js
+++ admin/main.js
@@ -1,48 +1,147 @@
-// admin.js
+document.addEventListener("DOMContentLoaded"),
+    function () {
+        const apiBaseUrl = 'http://localhost:3000';
 
-const apiUrl = "http://tu-api.com"; // Cambia esto a la URL real de tu API
+        //  Funciones para operaciones de actualizacion y eliminacion
 
-async function manejarOperacion(tipoOperacion, entidad) {
-    const entityId = document.getElementById(`${entidad}Id`).value;
-    const url = `${apiUrl}/api/${entidad}/${entityId}`;
-    const method = tipoOperacion === 'eliminar' ? 'DELETE' : 'PATCH';
+        function manejarOperacion(tipoOperacion, entidad) {
+            const entityId = document.getElementById('${entidad}Id').value;
 
-    let data = {};
+            //Actulizar y eliminar empresa
+            if (entidad === 'company') {
+                const companyDescription = document.getElementById('${entidad}Description').value;
+                const companyImageUrl = document.getElementById('${entidad}ImageUrl').value;
 
-    if (tipoOperacion === 'actualizar') {
-        if (entidad === 'company') {
-            data = {
-                description: document.getElementById(`${entidad}Description`).value,
-                imageUrl: document.getElementById(`${entidad}ImageUrl`).value,
-                // Agregar más campos según sea necesario para la actualización
-            };
-        } else if (entidad === 'service') {
-            data = {
-                description: document.getElementById(`${entidad}Description`).value,
-                url: document.getElementById(`${entidad}Url`).value,
-                // Agregar más campos según sea necesario para la actualización
-            };
-        } else if (entidad === 'galery') {
-            data = {
-                description: document.getElementById(`${entidad}Description`).value,
-                url: document.getElementById(`${entidad}Url`).value,
-                // Agregar más campos según sea necesario para la actualización
-            };
+                if (tipoOperacion === 'actualizar') {
+                    updateCompany(entityId, companyImageUrl);
+                } else if (tipoOperacion === 'eliminar') {
+                    deleteEntity(entityId, 'company');
+                }
+            }
+
+            //Actualizar y eliminar servicio
+            else if (entidad === 'service') {
+                const serviceDescription = document.getElementById('${entidad}Description').value;
+
+                const serviceUrl = document.getElementById('${entidad}Url').value;
+
+                if (tipoOperacion === 'actualizar') {
+                    updateService(entityId, serviceDescription, serviceUrl);
+                } else if (tipoOperacion === 'eliminar') {
+                    deleteEntity(entityId, 'service');
+                }
+            }
+
+
+            //Actualizar y eliminar galeria
+            else if (entidad === 'galery') {
+                const galeryDescription = document.getElementById('${entidad}Description').value;
+
+                const galeryUrl = document.getElementById('${entidad}Url').value;
+
+                if (tipoOperacion === 'actualizar') {
+                    updateGalery(entityId, galeryDescription, galeryUrl);
+                } else if (tipoOperacion === 'eliminar') {
+                    deleteEntity(entityId, 'galery');
+                }
+            }
         }
-    }
 
-    const response = await fetch(url, {
-        method,
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
-    });
+        // Función para actualizar entidad (Company, Galery, Service)
+        function updateEntity(entityId, description, url) {
+            // Implementa la lógica para actualizar una entidad
+            console.log(`Actualizar entidad con ID: ${entityId}, Descripción: ${description}, URL: ${url}`);
+        }
 
-    // Manejar la respuesta según sea necesario
-    if (response.ok) {
-        console.log(`${tipoOperacion} ${entidad} exitoso.`);
-    } else {
-        console.error(`Error al ${tipoOperacion} ${entidad}.`);
-    }
-}
+        // Función para eliminar una entidad (Company, Galery, Service)
+        function deleteEntity(entityId, entityType) {
+            // Llamada a la API para eliminar la entidad
+            fetch(`${apiBaseUrl}/api/${entityType}s/${entityId}`, {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            })
+                .then(response => response.json())
+                .then(data => {
+                    console.log(`Respuesta del servidor (Eliminar ${entityType}):`, data);
+                    // Puedes realizar acciones adicionales según la respuesta del servidor
+                })
+                .catch(error => {
+                    console.error('Error:', error);
+                });
+        }
+        // Función para actualizar una empresa
+        function updateCompany(companyId, description, imageUrl) {
+            // Llamada a la API para actualizar la empresa
+            fetch(`${apiBaseUrl}/api/companies/${companyId}`, {
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    description: description,
+                    imageUrl: imageUrl,
+                }),
+            })
+                .then(response => response.json())
+                .then(data => {
+                    console.log('Respuesta del servidor (Actualizar empresa):', data);
+                    // Puedes realizar acciones adicionales según la respuesta del servidor
+                })
+                .catch(error => {
+                    console.error('Error:', error);
+                });
+        }
+
+        // Función para actualizar un servicio
+        function updateService(serviceId, description, url) {
+            // Llamada a la API para actualizar el servicio
+            fetch(`${apiBaseUrl}/api/services/${serviceId}`, {
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    description: description,
+                    url: url,
+                }),
+            })
+                .then(response => response.json())
+                .then(data => {
+                    console.log('Respuesta del servidor (Actualizar servicio):', data);
+                    // Puedes realizar acciones adicionales según la respuesta del servidor
+                })
+                .catch(error => {
+                    console.error('Error:', error);
+                });
+        }
+
+        // Función para actualizar una galería
+        function updateGalery(galeryId, description, url) {
+            // Llamada a la API para actualizar la galería
+            fetch(`${apiBaseUrl}/api/galerys/${galeryId}`, {
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    description: description,
+                    url: url,
+                }),
+            })
+                .then(response => response.json())
+                .then(data => {
+                    console.log('Respuesta del servidor (Actualizar galería):', data);
+                    // Puedes realizar acciones adicionales según la respuesta del servidor
+                })
+                .catch(error => {
+                    console.error('Error:', error);
+                });
+        }
+    };
+
+
+
+
+
